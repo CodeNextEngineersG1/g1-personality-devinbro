474a3e7410a99f88c16ee5abdbd523f9b8a3ad2d
devinbro <32602563+devinbro@users.noreply.github.com>
2018-04-21 13:04:19 -0700

    gg no reeeeeeeeeee

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+let food= null;
+let goal= null;
+let technique = null;
+let pageTitle = document.getElementById("page-title");
+let pageTitleText = pageTitle.innerHTML;
+let tryAgain = document.getElementById("try-again")
+let quizWrapper =
+let result =
+let formSubmit
